429d836105e05bd75d768cf31a2359c1409b8d6e
Rafig Pashayev <pashayevler@gmail.com>
2024-10-08 22:21:53 +0400

    fix dumper

--- dump/dumper.py
+++ dump/dumper.py
@@ -10,23 +10,51 @@ def dump_postgresql_to_sql(output_file):
             for table in tables:
                 table_name = table[0]
                 print("table > ", table_name)
-                
-                # CREATE TABLE for table
+                table_name_for_sql = f'"{table_name}"' if table_name == "order" else table_name
+                # CREATE TABLE
                 cursor.execute(f"""
-                    SELECT 'CREATE TABLE ' || table_name || ' (' || string_agg(column_name || ' ' || data_type, ', ') || ');'
+                    SELECT 'CREATE TABLE ' || '{table_name_for_sql}' || ' (' || 
+                        string_agg(
+                            CASE 
+                                WHEN column_name = 'default' THEN '\"' || column_name || '\" ' || data_type
+                                ELSE column_name || ' ' || data_type 
+                            END, 
+                        ', ' ORDER BY ordinal_position) || 
+                        ');'
                     FROM information_schema.columns
                     WHERE table_name='{table_name}'
                     GROUP BY table_name;
                 """)
+
                 create_table_query = cursor.fetchone()[0]
+
+                
                 f.write(create_table_query + "\n")
 
                 # INSERT table datas
-                cursor.execute(f"SELECT * FROM {'\"order\"' if table_name == "order" else table_name};")
+                cursor.execute(f"SELECT * FROM {table_name_for_sql};")
                 rows = cursor.fetchall()
                 for row in rows:
-                    values = ', '.join([f"'{str(value)}'" if value is not None else 'NULL' for value in row])
-                    insert_query = f"INSERT INTO {table_name} VALUES ({values});"
+                    values = ""
+                    for value in row:
+                        if value is not None:
+                            if isinstance(value, dict):
+                                value = str(value)
+                                str_value = str(value.replace("'","\""))
+                                values += f", '{str_value}'"
+                            elif isinstance(value, bool):
+                                values += f", '{'TRUE' if value else 'FALSE'}'"
+                            elif isinstance(value, int):
+                                values += f", {value}"
+                            elif isinstance(value, float):
+                                values += f", {value}"
+                            else:
+                                str_value = str(value)
+                                values += f", \'{str_value}\'"
+                        else:
+                            values += ", NULL"
+                    values = values.lstrip(", ")
+                    insert_query = f"INSERT INTO \"{table_name}\" VALUES ({values});"
                     f.write(insert_query + "\n")
         
         print(f"Database dump has been written to {output_file}")
