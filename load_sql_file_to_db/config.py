852bd8cf00b605416bdb92d0f29d860e371febcb
Rafig Pashayev <pashayevler@gmail.com>
2024-10-08 19:52:58 +0400

    change name

---
+++ load_sql_file_to_db/config.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+load_dotenv()
+
+POSTGRES_DB_LOAD = os.getenv("POSTGRES_DB_LOAD")
+POSTGRES_USER_LOAD = os.getenv("POSTGRES_USER_LOAD")
+POSTGRES_PASSWORD_LOAD = os.getenv("POSTGRES_PASSWORD_LOAD")
+POSTGRES_HOST_LOAD = os.getenv("POSTGRES_HOST_LOAD")
+POSTGRES_PORT_LOAD = os.getenv("POSTGRES_PORT_LOAD")
+INPUT_FILE = "backup_postgresql.sql"
+
+
+connection = psycopg2.connect(
+            database=POSTGRES_DB_LOAD,
+            user=POSTGRES_USER_LOAD,
+            password=POSTGRES_PASSWORD_LOAD,
+            host=POSTGRES_HOST_LOAD,
+            port=POSTGRES_PORT_LOAD
+        )
+cursor = connection.cursor()
---
+++ load_sql_file_to_db/load.py
@@ -0,0 +1,27 @@
+from config import *
+import psycopg2
+
+
+def load_sql_file_to_db(sql_file):
+    try:
+
+        # Open and read the sql file
+        with open(sql_file, 'r') as file:
+            sql_commands = file.read()
+
+        cursor.execute(sql_commands)
+
+        # Update changes
+        connection.commit()
+        print(f"Data from the '{sql_file}' file has been successfully loaded.")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+load_sql_file_to_db(sql_file=INPUT_FILE)
